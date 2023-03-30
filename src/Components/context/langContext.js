5af128b674fcc0f82a6a51604116533b95257d29
Bouba <diarraboubaca@yahoo.fr>
2023-03-31 01:20:25 +0200

    Changement de la langue au click et repérage de la langue du navigateur

--- src/Components/context/langContext.js
+++ src/Components/context/langContext.js
@@ -2,11 +2,24 @@ import React, {createContext, useState} from "react";
 
 export const Context = createContext();
 
+const supportedLangs = ['EN', 'FR', 'ES'];
+
+let browserLang = navigator.language.slice(0, 2).toUpperCase();
+
+if(supportedLangs.indexOf(browserLang) === -1) {
+    console.log("not supported");
+    browserLang = "EN";
+}
+
 const ContextProvider = props => {
-    const [lang, setLang] = useState('EN');
+    const [lang, setLang] = useState(browserLang);
+
+    const toggleLang = changeLange => {
+        setLang(changeLange)
+    }
 
     return (
-        <Context.Provider value={{lang}}>
+        <Context.Provider value={{lang, toggleLang}}>
             {props.children}
         </Context.Provider>
     )
